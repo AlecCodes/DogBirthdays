042cae1fb87bf565d499839ea04f1e6a6fa264cd
Alec Hannaford <alec322@gmail.com>
2023-06-09 14:41:08 -0700

    inital commit

---
+++ readme.md
@@ -0,0 +1,2 @@
+###Notes
+- Command used to init table: "CREATE TABLE dogs ( dog_id serial PRIMARY KEY, name VARCHAR(40) NOT NULL, birthday DATE);"
---
+++ src/main/java/com/example/dogbirthdays/DogBirthdaysApplication.java
@@ -0,0 +1,13 @@
+package com.example.dogbirthdays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogBirthdaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogBirthdaysApplication.class, args);
+	}
+
+}
